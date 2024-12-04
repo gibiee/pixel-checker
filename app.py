164fa3de80f3340231e3b13d033194971548d9bb
gibiee <gibiee@naver.com>
2024-12-04 23:12:18 +0900

    show plot

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ import numpy as np
 import pandas as pd
 from collections import Counter
 
+img = Image.open('color_map.png').convert('RGB')
+img_array = np.array(img)
+
+mask = img.convert('L')
+mask_bool = np.where(np.array(mask) > 128, True, False)
+
+rgb_pixels = img_array[mask_bool]
+
+r_counter = Counter(rgb_pixels[:, 0].tolist())
+g_counter = Counter(rgb_pixels[:, 1].tolist())
+b_counter = Counter(rgb_pixels[:, 2].tolist())
+
+dict = {
+    'value': list(range(256)),
+    'R_count': [r_counter.get(i, 0) for i in range(256)],
+    'G_count': [g_counter.get(i, 0) for i in range(256)],
+    'B_count': [b_counter.get(i, 0) for i in range(256)]
+}
+df = pd.DataFrame(dict)
+df
+
 js_func = """
 function refresh() {
     const url = new URL(window.location);
@@ -21,7 +42,7 @@ def make_tuple(pixel) :
     else :
         return pixel
 
-def check_pixel_value(pil_img: Image, evt: gr.SelectData):
+def check_pixel_value(pil_img: Image.Image, evt: gr.SelectData):
     pointX, pointY = evt.index
 
     pil_rgb = np.array(pil_img.convert('RGB'))[pointY, pointX]
@@ -42,53 +63,26 @@ def check_pixel_value(pil_img: Image, evt: gr.SelectData):
     return [make_tuple(pixel) for pixel in pixels]
 
 def extract_from_editor(editor) :
-    print(editor)
-    img = editor['background'].convert('RGB')
+    img = editor['background']
     mask = editor['layers'][0].split()[3]
-    return img, mask
+    if len(np.unique(mask)) > 1 : img.putalpha(mask)
+    return img
 
-def analysis_pixel_value(img: Image, mask: Image) :
+def analysis_pixel_value(img_rgba: Image.Image) :
     img_rgb, img_hsv, mask = np.array(img.convert('RGB')), np.array(img.convert('HSV')), np.array(mask)
     mask_bool = np.where(mask > 128, True, False)
     
     rgb_pixels = img_rgb[mask_bool]
     hsv_pixels = img_hsv[mask_bool]
 
-    r_count = Counter(rgb_pixels[:, 0].tolist())
-    g_count = Counter(rgb_pixels[:, 1].tolist())
-    b_count = Counter(rgb_pixels[:, 2].tolist())
 
-    h_count = Counter(hsv_pixels[:, 0].tolist())
-    s_count = Counter(hsv_pixels[:, 1].tolist())
-    v_count = Counter(hsv_pixels[:, 2].tolist())
-
-img = Image.open('color_map.png').convert('RGB')
-img_array = np.array(img)
-
-mask = img.convert('L')
-mask_bool = np.where(np.array(mask) > 128, True, False)
-
-rgb_pixels = img_array[mask_bool]
-
-r_count = Counter(rgb_pixels[:, 0].tolist())
-g_count = Counter(rgb_pixels[:, 1].tolist())
-b_count = Counter(rgb_pixels[:, 2].tolist())
-
-t = {
-    0: 0,
-    1: 10,
-    3: 30
-}
-pd.DataFrame(t, index=['T'])
-
-pd.DataFrame(r_count, index=['R'])
 
 with gr.Blocks(js=js_func) as demo :
 
     with gr.Tab("Check pixel value") :
         with gr.Row() :
             input_img = gr.Image(sources=['upload'], type='pil', image_mode='RGB', scale=3,
-                                show_label=False, show_download_button=False, show_share_button=False)
+                                 show_label=False, show_download_button=False, show_share_button=False)
 
             with gr.Column(scale=1):
                 pil_rgb = gr.Textbox(label='PIL RGB', interactive=False)
@@ -103,21 +97,25 @@ with gr.Blocks(js=js_func) as demo :
                 opencv_lab = gr.Textbox(label='OpenCV Lab', interactive=False)
                 opencv_grayscale = gr.Textbox(label='OpenCV Grayscale', interactive=False)
 
-        # gr.examples()
+        gr.Examples(examples=['color_map.png'], inputs=input_img)
         input_img.select(check_pixel_value, inputs=[input_img], outputs=[pil_rgb, pil_hsv, pil_grayscale, opencv_bgr, opencv_rgb, opencv_hsv, opencv_hsv_full, opencv_lab, opencv_grayscale])
+
 
     with gr.Tab("Distribution Analysis") :
         with gr.Row() :
             input_img = gr.ImageEditor(sources=['upload'], type='pil', image_mode='RGBA', layers=False,
                                        show_label=False, show_download_button=False, show_share_button=False,
-                                       brush=gr.Brush(colors=["#AAAAAA"], color_mode="fixed"))
+                                       transforms=[], brush=gr.Brush(colors=['AAAAAA'], default_color='AAAAAA', color_mode='fixed'))
             
-            extract_img = gr.Image(type='pil', image_mode='RGB', show_label=False, show_download_button=False, show_share_button=False)
-            extract_mask = gr.Image(type='pil', image_mode='RGB', show_label=False, show_download_button=False, show_share_button=False)
+            cut_img = gr.Image(type='pil', image_mode='RGBA', show_label=False, show_download_button=False, show_share_button=False)
 
         btn = gr.Button('Analysis', variant='primary')
+        btn.click(fn=extract_from_editor, inputs=[input_img], outputs=[cut_img])
 
-        btn.click(fn=extract_from_editor, inputs=[input_img], outputs=[extract_img, extract_mask])
+    with gr.Row() :
+        gr.BarPlot(df, x='value', y='R_count', x_lim=[0, 255], x_bin=10)
+        gr.BarPlot(df, x='value', y='G_count', x_lim=[0, 255], x_bin=10)
+        gr.BarPlot(df, x='value', y='B_count', x_lim=[0, 255], x_bin=10)
 
 
 
